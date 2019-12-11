953c50d19d8316fa7fd5bdf8678be855739e7d45
Erok <50800579+MykhailoPetrenko@users.noreply.github.com>
2019-12-11 21:28:12 +0200

    Update SkladnikController.cs

--- Controllers/SkladnikController.cs
+++ Controllers/SkladnikController.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PizzaAwesomeApi.Models;
 
 namespace PizzaAwesomeApi.Controllers
@@ -12,7 +13,7 @@ namespace PizzaAwesomeApi.Controllers
     [ApiController]
     public class SkladnikController : ControllerBase
     {
-        s17006Context _context;
+        private readonly s17006Context _context;//readonly - mozemy nadac wartosc tylko w kostr
         public SkladnikController(s17006Context context)
         {
             _context = context;
@@ -29,7 +30,7 @@ namespace PizzaAwesomeApi.Controllers
             if (skladnik == null) return NotFound();
             return Ok(skladnik);
         }
-        [HttpDelete("delete/{id:int}")]
+  /*      [HttpDelete("delete/{id:int}")]
         public IActionResult DeleteSkladnik(int id)
         {
             var skladnik = _context.Skladnik.Where(i => i.IdSkladnik == id).FirstOrDefault();
@@ -37,6 +38,33 @@ namespace PizzaAwesomeApi.Controllers
             _context.Skladnik.Remove(skladnik);
             _context.SaveChanges();
             return Ok(skladnik + " Zostal usuniety");
+        }*/
+        [HttpPost]
+        public IActionResult Create(Skladnik newSkladnik)
+        {
+            _context.Skladnik.Add(newSkladnik);
+            _context.SaveChanges();
+
+            return StatusCode(201, newSkladnik);
+        }
+        [HttpPut]
+        public IActionResult Update(Skladnik updateSkladnik)
+        {
+                        if (_context.Skladnik.Count(s => s.IdSkladnik == updateSkladnik.IdSkladnik) != 1) return NotFound();
+            _context.Skladnik.Attach(updateSkladnik);
+            _context.Entry(updateSkladnik).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(updateSkladnik);
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var skladnik = _context.Skladnik.FirstOrDefault(i => i.IdSkladnik == id);
+            if (skladnik == null) return NotFound();
+            _context.Skladnik.Remove(skladnik);
+            _context.SaveChanges();
+            return Ok(skladnik);
         }
     }
 }
